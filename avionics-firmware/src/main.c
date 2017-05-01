6e4611f3d03c7ed9c20e31502cad327a7afbf5a8
Michael Spieler <michael.spieler@epfl.ch>
2017-05-01 18:48:28 +0200

    Avionics: windtunnel test firmware

--- avionics-firmware/src/main.c
+++ avionics-firmware/src/main.c
@@ -28,10 +28,15 @@ void heartbeat_main(void *arg)
 #define PRESSURE_SENSOR_STATUS_STALE_DATA   0b10
 #define PRESSURE_SENSOR_STATUS_DIAGNOSTIC   0b11
 
-#define PRESSURE_SENSOR_MAX     6894.76f    /* [Pa] */
-#define PRESSURE_SENSOR_MIN     -6894.76f   /* [Pa] */
+/* HSCMRRN001PD2A3, +/- 1psi */
+#define PRESSURE_SENSOR1_MAX     6894.76f   /* [Pa] */
+#define PRESSURE_SENSOR1_MIN     -6894.76f  /* [Pa] */
 
-int differential_pressure_read(float *p_press, float *p_temp)
+/* SSCMRNN015PG5A3 0 - 15 psi*/
+#define PRESSURE_SENSOR2_MAX     103421.f    /* [Pa] */
+#define PRESSURE_SENSOR2_MIN     0.f         /* [Pa] */
+
+int low_range_differential_pressure_read(float *p_press, float *p_temp)
 {
     uint8_t buf[4];
     msg_t ret;
@@ -52,8 +57,38 @@ int differential_pressure_read(float *p_press, float *p_temp)
     }
 
     /* Pressure transfer function */
-    *p_press = ((float)press - 1638) * (PRESSURE_SENSOR_MAX - PRESSURE_SENSOR_MIN)
-                / (14745 - 1638) + PRESSURE_SENSOR_MIN;
+    *p_press = ((float)press - 1638) * (PRESSURE_SENSOR1_MAX - PRESSURE_SENSOR1_MIN)
+                / (14745 - 1638) + PRESSURE_SENSOR1_MIN;
+
+    /* Temperature transfer function */
+    *p_temp = ((float)temp / 2047 * 200) - 50;
+
+    return 0;
+}
+
+int high_range_differential_pressure_read(float *p_press, float *p_temp)
+{
+    uint8_t buf[4];
+    msg_t ret;
+    i2cAcquireBus(&I2CD2);
+    ret = i2cMasterReceiveTimeout(&I2CD2, DIFF_PRESS_HIGH_RANGE_SENSOR_ADDR,
+                                     buf, sizeof(buf), TIME_INFINITE);
+    i2cReleaseBus(&I2CD2);
+
+    if (ret != MSG_OK) {
+        return -1;
+    }
+    uint16_t press = (((uint16_t)buf[0] << 8) | (uint16_t)buf[1]) & 0x3fff;
+    uint16_t temp = (((uint16_t)buf[2] << 3) | ((uint16_t)buf[1] >> 5)) & 0x07ff;
+    uint8_t status = buf[0] >> 6;
+
+    if (status != PRESSURE_SENSOR_STATUS_NORMAL && status != PRESSURE_SENSOR_STATUS_STALE_DATA) {
+        return -1;
+    }
+
+    /* Pressure transfer function */
+    *p_press = ((float)press - 1638) * (PRESSURE_SENSOR2_MAX - PRESSURE_SENSOR2_MIN)
+                / (14745 - 1638) + PRESSURE_SENSOR2_MIN;
 
     /* Temperature transfer function */
     *p_temp = ((float)temp / 2047 * 200) - 50;
@@ -92,17 +127,49 @@ int main(void)
     palClearPad(GPIOB, GPIOB_DIF_P_SENS_EN_N);
     chThdSleepMilliseconds(10);
 
+    chprintf(xbee, "p1,t1,p2,t2,raw1,raw2\n");
+
     while (true) {
         float p, t;
         int err;
 
-        err = differential_pressure_read(&p, &t);
+        err = low_range_differential_pressure_read(&p, &t);
         if (!err) {
-            chprintf(xbee, "pressure %f, temperature %f\n", p, t);
+            chprintf(xbee, "%f,%d", p, (int)t);
         } else {
-            chprintf(xbee, "pressure read error\n");
+            chprintf(xbee, ",");
         }
+        chprintf(xbee, ",");
 
-        chThdSleepMilliseconds(100);
+        err = high_range_differential_pressure_read(&p, &t);
+        if (!err) {
+            chprintf(xbee, "%f,%d", p, (int)t);
+        } else {
+            chprintf(xbee, ",");
+        }
+        chprintf(xbee, ", ");
+
+        uint32_t buf[2];
+        i2cAcquireBus(&I2CD2);
+        err = i2cMasterReceiveTimeout(&I2CD2, DIFF_PRESS_LOW_RANGE_SENSOR_ADDR,
+                                         (void*)buf, sizeof(buf), TIME_INFINITE);
+        i2cReleaseBus(&I2CD2);
+
+        if (err == MSG_OK) {
+            chprintf(xbee, "%08x %08x", buf[0], buf[1]);
+        }
+        chprintf(xbee, ", ");
+
+        i2cAcquireBus(&I2CD2);
+        err = i2cMasterReceiveTimeout(&I2CD2, DIFF_PRESS_HIGH_RANGE_SENSOR_ADDR,
+                                         (void*)buf, sizeof(buf), TIME_INFINITE);
+        i2cReleaseBus(&I2CD2);
+
+        if (err == MSG_OK) {
+            chprintf(xbee, "%08x %08x", buf[0], buf[1]);
+        }
+        chprintf(xbee, "\n");
+
+        chThdSleepMilliseconds(50);
     }
 }
