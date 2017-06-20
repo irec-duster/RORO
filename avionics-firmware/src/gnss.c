62ca393ebeda5ec16ce51f26005435a5e4912502
Michael Spieler <michael.spieler@epfl.ch>
2017-06-20 18:03:35 +0200

    GNSS parse and publish

--- avionics-firmware/src/gnss.c
+++ avionics-firmware/src/gnss.c
@@ -1,28 +1,30 @@
 #include <ch.h>
 #include <hal.h>
+#include <string.h>
+
 #include "main.h"
 #include "gnss.h"
 
 /*
-Enable:
-GPIO: GPIOC_GPS_EN_N
+   Enable:
+   GPIO: GPIOC_GPS_EN_N
 
-Antenna switch:
-GPIO: GPIOC_GPS_SWITCH
-CTRL = HIGH: RF1, P33, up
-CTRL = LOW: RF2, P34, down
-*/
+   Antenna switch:
+   GPIO: GPIOC_GPS_SWITCH
+   CTRL = HIGH: RF1, P33, up
+   CTRL = LOW: RF2, P34, down
+ */
 
 /*
-baudrate: 19200
+   baudrate: 19200
 
-Messages:
+   Messages:
 
-UBX NAV SOL
-UBX NAV CLK
-UBX RXM RAWX
+   UBX NAV SOL
+   UBX NAV CLK
+   UBX RXM RAWX
 
-*/
+ */
 
 BaseSequentialStream *gnss = NULL;
 static void gnss_uart_init(void)
@@ -37,13 +39,61 @@ static void gnss_uart_init(void)
     gnss = (BaseSequentialStream *)&GNSS_SERIAL;
 }
 
+bool buffer_push_byte(char **wp, const char *end, char c)
+{
+    if (*wp < end) {
+        *(*wp)++ = c;
+    } else {
+        return true;
+    }
+    return false;
+}
+
+bool nmea_gngga_update = false;
+char gngga_sentence[GNSS_NMEA_MAX_SENTENCE];
+bool nmea_gngll_update = false;
+char gngll_sentence[GNSS_NMEA_MAX_SENTENCE];
+
 static THD_WORKING_AREA(gnss_thread, 2000);
 void gnss_main(void *arg)
 {
     (void)arg;
 
+    static char buf[GNSS_NMEA_MAX_SENTENCE];
+    const char *buf_end = &buf[0] + sizeof(buf) - 1;
+    char *wp = &buf[0];
+
+    bool nmea_sentence_found = false;
     while (1) {
-        chThdSleepMilliseconds(100);
+        char c = sdGet(&GNSS_SERIAL);
+        if (c == '$') {
+            nmea_sentence_found = true;
+            wp = &buf[0];
+        }
+        if (nmea_sentence_found) {
+            if (buffer_push_byte(&wp, buf_end, c)) {
+                // overflow
+                nmea_sentence_found = false;
+                continue;
+            }
+
+            // end of sentence
+            if (c == '\n' || c == '\r') {
+
+                *wp = 0; // 0-terminate
+
+                if (strcmp(buf, "$GNGGA")) {
+                    strcpy(gngga_sentence, buf);
+                    nmea_gngga_update = true;
+                } else if (strcmp(buf, "$GNGLL")) {
+                    strcpy(gngll_sentence, buf);
+                    nmea_gngll_update = true;
+                }
+                // XXX TODO: add more NMEA messages here.
+
+                nmea_sentence_found = false;
+            }
+        }
     }
 }
 
--- avionics-firmware/src/gnss.h
+++ avionics-firmware/src/gnss.h
@@ -7,6 +7,13 @@
 extern BaseSequentialStream *gnss;
 #define GNSS_SERIAL SD6
 
+#define GNSS_NMEA_MAX_SENTENCE 200
+
+extern bool nmea_gngga_update;
+extern char nmea_gngga_sentence[GNSS_NMEA_MAX_SENTENCE];
+extern bool nmea_gngll_update;
+extern char nmea_gngll_sentence[GNSS_NMEA_MAX_SENTENCE];
+
 void gnss_switch_upper_antenna(void);
 void gnss_switch_lower_antenna(void);
 
